eb07ebeb0813a77de5a7f40e0c23fa42c99512fe
s26496 <s26496@pjwstk.edu.pl>
2023-03-10 18:50:40 +0100

    second commit

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import webbrowser
+import requests as requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("hello world")
+    pageurl = input("Podaj stronę internetową: ")
+    date = input("Podaj datę w formacie rok miesiąc dzień, np. 20060101: ")
+    url = "http://archive.org/wayback/available?url="+pageurl+"&timestamp="+str(date)
+    response = requests.get(url)
+    d = response.json()
+    page = d["archived_snapshots"]["closest"]["url"]
+    webbrowser.open(page)
